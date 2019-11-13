50c95cbeb5f798896cd610b137c9e65c5e93bb34
jikish <jikish2103dk@gmail.com>
2019-11-13 12:19:14 +0600

    Part-1 Task-5

---
+++ task-5.py
@@ -0,0 +1,9 @@
+def string_length(x): return x
+def test_string_length():
+    assert string_length('hello!') == 6
+    assert string_length('banana') == 6
+    assert string_length('8') == 1
+    assert string_length('funnyguys') == 9
+    assert string_length('101') == 3
+print("YOUR CODE IS CORRECT!")
+test_string_length()
